e092b2bb90b1bdd8a0c1a8b1bcf2ee102007cac0
dubingabie <gaberiel.dubin@mail.huji.ac.il>
2024-11-20 13:17:10 +0200

    score manager

--- Assets/Scenes/ScoreManager.cs
+++ Assets/Scenes/ScoreManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 // This will be a singleton manager for the score
-
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
@@ -27,16 +26,43 @@ public class ScoreManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
-        // Check if scoreText is assigned
-        if (scoreText == null)
-        {
-            Debug.LogError("Score Text is not assigned to ScoreManager!");
-        }
     }
 
     void Start()
     {
+        // Create Canvas if it doesn't exist
+        Canvas canvas = FindObjectOfType<Canvas>();
+        if (canvas == null)
+        {
+            GameObject canvasObject = new GameObject("Canvas");
+            canvas = canvasObject.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            
+            canvasObject.AddComponent<CanvasScaler>();
+            canvasObject.AddComponent<GraphicRaycaster>();
+        }
+
+        // Create Text if not assigned
+        if (scoreText == null)
+        {
+            GameObject textObject = new GameObject("ScoreText");
+            textObject.transform.SetParent(canvas.transform);
+            scoreText = textObject.AddComponent<Text>();
+            
+            // Set default properties
+            scoreText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            scoreText.fontSize = 24;
+            scoreText.color = Color.white;
+            scoreText.text = "Diamonds: 0";
+            
+            // Position the text in the top-left corner
+            RectTransform rectTransform = textObject.GetComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(0, 1);
+            rectTransform.anchorMax = new Vector2(0, 1);
+            rectTransform.pivot = new Vector2(0, 1);
+            rectTransform.anchoredPosition = new Vector2(10, -10);
+        }
+        
         UpdateScoreText();
     }
 
